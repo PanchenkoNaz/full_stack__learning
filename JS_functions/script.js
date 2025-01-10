f6a682467833c819f1ab9294939c1723542cbaa5
Nazar <nazarpanchenko1997@gmail.com>
2025-01-10 13:36:36 +0100

    js functions

---
+++ JS_functions/script.js
@@ -0,0 +1,82 @@
+// function
+
+// function showMessage (name, city) {
+//     console.log("Hello my name is " + name + " I'm from " + city);
+// }
+
+// showMessage("Nick", "Kyiv");
+// showMessage("Simon", "Berlin");
+
+
+
+// const userName = "Nazar";
+
+// const sayHi = function () {
+//     let message = "Hello, my name is "
+//     console.log(message  + userName);
+    
+// }
+
+// sayHi();
+
+
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// let result = sum(7, 8);
+// console.log(result);
+
+
+// const testFunc = function (a, b) {
+//     console.log("a :", a);
+//     console.log("b :", b);
+//     c = a + b;
+
+//     return c;
+// };
+
+// testFunc(5, 8);
+
+
+
+// const a = function () {
+//     c();
+//     console.log("function a");
+//     b();
+// };
+
+// const b = function () {
+//     console.log("function b");
+// };
+
+// const c = function () {
+//     console.log("function c");
+// };
+
+// a();
+
+
+
+
+// const addName = function () {
+//     const arg = Array.from(arguments);
+//     console.log(arguments);
+//     console.log(arg);
+    
+// };
+
+// addName(1, 3, 5);
+
+// addName(1, 3, 5, 67, 55);
+
+
+
+
+
+
+
+
+
+
