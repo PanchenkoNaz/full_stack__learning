7857714092daa037442f78913da18382230c43a3
Nazar <nazarpanchenko1997@gmail.com>
2025-01-10 14:33:35 +0100

    js functions

--- JS_functions/script.js
+++ JS_functions/script.js
@@ -3,41 +3,40 @@
 // function showMessage (name, city) {
 //     console.log("Hello my name is " + name + " I'm from " + city);
 // }
-
 // showMessage("Nick", "Kyiv");
 // showMessage("Simon", "Berlin");
 
 
 
-// const userName = "Nazar";
 
+// const userName = "Nazar";
 // const sayHi = function () {
 //     let message = "Hello, my name is "
 //     console.log(message  + userName);
-    
 // }
-
 // sayHi();
+
 
 
 
 // function sum(a, b) {
 //     return a + b;
 // }
-
 // let result = sum(7, 8);
 // console.log(result);
+
+
 
 
 // const testFunc = function (a, b) {
 //     console.log("a :", a);
 //     console.log("b :", b);
 //     c = a + b;
-
 //     return c;
 // };
-
 // testFunc(5, 8);
+
+
 
 
 
@@ -54,7 +53,6 @@
 // const c = function () {
 //     console.log("function c");
 // };
-
 // a();
 
 
@@ -64,15 +62,128 @@
 //     const arg = Array.from(arguments);
 //     console.log(arguments);
 //     console.log(arg);
-    
 // };
-
 // addName(1, 3, 5);
-
 // addName(1, 3, 5, 67, 55);
 
 
 
+// const addName = function (...arg) {
+//     console.log(arg);
+// };
+// addName(1, 3, 5);
+// addName(1, 3, 5, 54, 91);
+
+
+
+
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) {
+//         yes();
+//     } else {
+//         no();
+//     }
+// }
+// function showOk() {
+//     console.log("You say ok");
+// }
+// function showCancel() {
+//     console.log("You say No");
+// }
+// ask("Yes or no?", showOk, showCancel);
+
+
+
+
+// const hello = function (name) {
+//     console.log(`Hello, ${name}`);
+// };
+// const searchName = function (callback) {
+//     const name = "Nick";
+//     callback(name);
+// };
+// searchName(hello);
+
+
+
+
+
+// function checkAge(age) {
+//     if (age > 18) {
+//         return console.log("Hello");
+//     }
+//        return console.log("Good bye");
+// }
+// checkAge(12);
+
+
+
+
+// function showMovie(age) {
+//     if (age < 21) {
+//         return
+//     }
+//     return console.log("Go to movie");
+// }
+// showMovie(11);
+
+
+
+
+
+// const test = (arg) => {
+//     console.log(arg);
+// };
+// test("hello");
+
+
+
+
+// const showMovie = (age) => {
+//     if (age < 21) {
+//         return console.log("Hello");
+//     }
+//     return console.log("Bye!");
+// };
+// showMovie(2);
+
+
+
+
+
+// const a = () => {
+//     console.log("a");
+// };
+
+// const b = () => {
+//     console.log("b");
+// };
+
+// const c = () => {
+//     console.log("c");
+//     a();
+//     b();
+// };
+// c();
+
+
+
+
+
+
+// const addNumber = (number) => {
+//     let summ = Number(number) + 10;
+//     return console.log(summ);
+// }
+
+// const numberRef = document.querySelector('input[name="number"]');
+
+// const buttonRef = document.querySelector("button");
+
+// buttonRef.addEventListener("click", () => {
+//     addNumber(numberRef.value);
+// });
 
 
 
@@ -80,3 +191,13 @@
 
 
 
+
+
+
+
+
+
+
+
+
+ 
