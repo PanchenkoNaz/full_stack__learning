ed314151910b10662d9e137b57ace12beab40c76
Nazar <nazarpanchenko1997@gmail.com>
2025-02-07 14:59:13 +0100

    webpack style loader

--- JS_asynchrony-data/webpack.config.js
+++ JS_asynchrony-data/webpack.config.js
@@ -9,4 +9,12 @@ module.exports = {
         clean: true,
     },
     plugins: [new HtmlWebpackPlugin()],
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
