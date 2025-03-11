d93a2995e1197da6296b1cec4ff1cfcfe1151e44
Nazar <nazarpanchenko1997@gmail.com>
2025-03-11 17:18:48 +0100

    react state, events

---
+++ LESSON16_{React_state-events}/react-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import HelloWorldComponent from './HelloWorldComponent';
+import MyClassComponent from './MyClassComponent';
+import './App.css';
+
+function App() {
+  const hello = "Hello World";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {hello}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {<HelloWorldComponent />}
+        <MyClassComponent/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
